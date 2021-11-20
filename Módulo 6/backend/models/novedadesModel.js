c184564c6a7567fab8e90a8b0e302dae41d19f0e
Martín Simonetti <msimonetti@gmail.com>
2021-11-20 17:12:38 -0300

    Se agregó el eliminar y modificar a las novedades.

--- Módulo 6/backend/models/novedadesModel.js
+++ Módulo 6/backend/models/novedadesModel.js
@@ -21,4 +21,35 @@ async function insertNovedad(obj){
     }
 }
 
-module.exports = { getNovedades, insertNovedad };
+async function deleteNovedadesById(id){
+    try {
+        var query = 'DELETE FROM novedades WHERE id = ?';
+        var rows = await pool.query(query, [id]);
+        return rows;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+async function getNovedadById(id){
+    try {
+        var query = 'SELECT * FROM novedades WHERE id = ?';
+        var rows = await pool.query(query, [id]);
+        return rows[0];
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function modificarNovedadById(obj, id){
+    try {
+        var query = 'UPDATE novedades SET ? WHERE id = ?';
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = { getNovedades, insertNovedad, deleteNovedadesById, getNovedadById, modificarNovedadById };
--- Módulo 6/backend/routes/admin/novedades.js
+++ Módulo 6/backend/routes/admin/novedades.js
@@ -41,4 +41,53 @@ router.post('/agregar', async (req, res, next) => {
   }
 })
 
+router.get('/eliminar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades');
+})
+
+router.get('/modificar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  var novedad = await novedadesModel.getNovedadById(id);
+  // console.log(id);
+  // console.log(novedad);
+
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    novedad
+  })
+})
+
+router.post('/modificar', async (req, res, next) => {
+  try {
+    if (req.body.titulo != '' && req.body.subtitulo != '' && req.body.cuerpo != '') {
+      var obj = {
+        titulo: req.body.titulo,
+        subtitulo: req.body.subtitulo,
+        cuerpo: req.body.cuerpo
+      }
+
+      // console.log(res.body.id);
+      // console.log(obj);
+
+      await novedadesModel.modificarNovedadById(obj, req.body.id);
+      res.redirect('/admin/novedades');
+    } else {
+      res.render('admin/modificar', {
+        layout: 'admin/layout',
+        error: true,
+        message: 'Todos los campos son requeridos'
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se modificó la novedad'
+    });
+  }
+})
+
 module.exports = router;
